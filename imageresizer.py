4df88a460cf3f805de1b4b192dd31cba746a325f
E7271 <ethanvaincourt@gmail.com>
2019-01-23 10:43:31 -0800

    Create imageresizer.py

---
+++ imageresizer.py
@@ -0,0 +1,8 @@
+from PIL import Image
+import numpy as np
+
+IMAGE_DIR = "C:/Users/Ethan's Laptop/Desktop/Python/IMAGE_DIR"
+image = Image.open("C:/Users/Ethan's Laptop/Desktop/Python/IMAGE_DIR/download.png").convert('L')
+resizedImage = image.resize((100,100))
+resizedImage.save("C:/Users/Ethan's Laptop/Desktop/Python/IMAGE_DIR/newimagedata.png") 
+array = np.array(resizedImage)
