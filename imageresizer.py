5e1eaf9fc2e91e5fb3e3e9c04dfdde00645854d4
E7271 <ethanvaincourt@gmail.com>
2019-01-23 16:17:45 -0800

    Update imageresizer.py

--- imageresizer.py
+++ imageresizer.py
@@ -3,6 +3,6 @@ import numpy as np
 
 IMAGE_DIR = "C:/Users/Ethan's Laptop/Desktop/Python/IMAGE_DIR"
 image = Image.open("C:/Users/Ethan's Laptop/Desktop/Python/IMAGE_DIR/download.png").convert('L')
-resizedImage = image.resize((100,100))
+resizedImage = image.resize((28,28))
 resizedImage.save("C:/Users/Ethan's Laptop/Desktop/Python/IMAGE_DIR/newimagedata.png") 
 array = np.array(resizedImage)
